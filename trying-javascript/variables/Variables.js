353602a249d825153e1f3e028b90a2467ab4a97a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-27 22:01:51 +0530

    Create Variables.js

---
+++ trying-javascript/variables/Variables.js
@@ -0,0 +1,15 @@
+// String data type
+const firstName = "Sudarshan"
+const lastName = "Mhasrup"
+
+// Number data type
+const age = 21
+const cgpa = 7.13
+
+// Boolean data type
+const isDeveloper = true
+
+console.log(`Name: ${firstName} ${lastName}.`)
+console.log(`Age: ${age} years.`)
+console.log(`CGPA: ${cgpa}.`)
+console.log(`Is Developer?: ${isDeveloper}.`)
